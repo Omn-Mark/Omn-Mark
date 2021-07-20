8e3ef19c61bf33f3ce548c554af56e2dd032e7cf
Omn-Mark <86652155+Omn-Mark@users.noreply.github.com>
2021-07-20 17:19:12 +0900

    mysql error1

--- login/server.js
+++ login/server.js
@@ -7,6 +7,7 @@ const fs = require('fs');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const session = require('express-session');
+const { connect } = require('http2');
 const filestore = require('session-file-store')(session);
 const connection = mysql.createConnection({
     host : 'us-cdbr-east-04.cleardb.com',
@@ -28,18 +29,25 @@ app.use(session({
 }))
 //app.use('/api', api);
 
-function handleDisconnect() {                            
-    connection.on('error', function(err) {
-      console.log('connection error', err);
-      if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
-        return handleDisconnect();                      
-      } else {                                    
-        throw err;                              
-      }
-    });
-  }
-  
-  handleDisconnect();
+function handleDisconnect() {
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 
 app.get('/', (req, res) => { 
     res.sendFile(__dirname + '/public/login.html');
